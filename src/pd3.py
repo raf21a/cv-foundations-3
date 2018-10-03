612a7611050fcef918508c300dfb804dedf6c6e5
Pedro Henrique Luz de Araujo <pedrohluzaraujo@gmail.com>
2018-10-03 15:24:50 -0300

    Remove referências a bônus e faz o programa ser chamado da raiz

--- src/pd3.py
+++ src/pd3.py
@@ -42,8 +42,6 @@ group.add_argument("--r2", action="store_true",
                     help="Requisito 2")
 group.add_argument("--r3", action="store_true",
                     help="Requisito 3")
-parser.add_argument("--bonus", action="store_true",
-                    help="Mapa de profundidade pela matriz fundamental")
 parser.add_argument("--img_l", nargs="?", default=None, metavar="path/to/file",
                     help="Caminho para a imagem da esquerda")
 parser.add_argument("--img_r", nargs="?", default=None, metavar="path/to/file",
@@ -96,14 +94,14 @@ def createDepthMap(imageL, imageR):
     if imageL is None or imageR is None:
         imageL = "../data/aloeL.png"
         print("Criando mapa de profundidade para aloe. Caso deseje a do bebe, use a opção -imageL and -imageR com as imagens correspondentes.")
-        leftImage = openImage("../data/aloeL.png")
-        rightImage = openImage("../data/aloeR.png")
+        leftImage = openImage("data/aloeL.png")
+        rightImage = openImage("/data/aloeR.png")
     else:
         leftImage = openImage(imageL)
         rightImage = openImage(imageR)
     dispMatrix, depthMatrix, _, _ = tuneParameters("r1", leftImage, rightImage)
-    saveImage("../" + os.path.basename(imageL).rstrip("L.png") + "_disp.png", dispMatrix)
-    saveImage("../" + os.path.basename(imageL).rstrip("L.png") + "_depth.png", depthMatrix)
+    saveImage(os.path.basename(imageL).rstrip("L.png") + "_disp.png", dispMatrix)
+    saveImage(os.path.basename(imageL).rstrip("L.png") + "_depth.png", depthMatrix)
 
 
 def getRotation(r1, r2):
@@ -115,8 +113,8 @@ def getTranslation(r, t1, t2):
 
 
 def depthFromHomography():
-    leftImage = openImage("../data/MorpheusL.jpg")
-    rightImage = openImage("../data/MorpheusR.jpg")
+    leftImage = openImage("data/MorpheusL.jpg")
+    rightImage = openImage("data/MorpheusR.jpg")
     rotation = getRotation(MORPHEUS_LEFT_ROTATION, MORPHEUS_RIGHT_ROTATION)
     translation = getTranslation(rotation, MORPHEUS_LEFT_TRANSLATION, MORPHEUS_RIGHT_TRANSLATION)
 
@@ -138,100 +136,39 @@ def depthFromHomography():
     cv2.waitKey(1)
 
     dispMatrix, depthMatrix, _, _ = tuneParameters("r2", leftReprojection, rightReprojection, q)
-    saveImage("../morpheus_disp.jpg", dispMatrix)
-    saveImage("../morpheus_depth.jpg", depthMatrix)
+    saveImage("morpheus_disp.jpg", dispMatrix)
+    saveImage("morpheus_depth.jpg", depthMatrix)
 
 
-def depthFromFundamentalMatrix():
-
-    #find possible matches
-    leftImage = openImage("../data/MorpheusL.jpg")
-    rightImage = openImage("../data/MorpheusR.jpg")
-
-    sift = cv2.xfeatures2d.SIFT_create()
-
-    kpl, descriptorLeft = sift.detectAndCompute(leftImage,None)
-    kpr, descriptorRight = sift.detectAndCompute(rightImage,None)
-
-    FLANN_INDEX_KDTREE = 0
-    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks=50)
-
-    flann = cv2.FlannBasedMatcher(index_params,search_params)
-    matches = flann.knnMatch(descriptorLeft,descriptorRight,k=2)
-
-    good = []
-    pts1 = []
-    pts2 = []
-
-    for i,(m,n) in enumerate(matches):
-        if m.distance < 0.8*n.distance:
-            good.append(m)
-            pts2.append(kpr[m.trainIdx].pt)
-            pts1.append(kpl[m.queryIdx].pt)
-    #possible matches: pts1 and pts2
-
-    pts1 = np.array(pts1)
-    pts2 = np.array(pts2)
-    F, mask = cv2.findFundamentalMat(pts1,pts2,cv2.FM_LMEDS)
-
-    # We select only inlier points
-    pts1 = pts1[mask.ravel()==1]
-    pts2 = pts2[mask.ravel()==1]
-
-    pts1 = np.int32(pts1)
-    pts2 = np.int32(pts2)
-
-    p1fNew = pts1.reshape((pts1.shape[0] * 2, 1))
-    p2fNew = pts2.reshape((pts2.shape[0] * 2, 1))
-    retBool ,rectmat1, rectmat2 = cv2.stereoRectifyUncalibrated(p1fNew,p2fNew,F,(2048,2048))
-    dst1 = cv2.warpPerspective(leftImage,rectmat1,(2048,2048))
-    dst2 = cv2.warpPerspective(rightImage,rectmat2,(2048,2048))
-
-    cv2.namedWindow('Teste 1', cv2.WINDOW_NORMAL)
-    cv2.namedWindow('Teste 2', cv2.WINDOW_NORMAL)
-
-    while(True):
-        cv2.imshow("Teste 1", dst1)
-        cv2.imshow("Teste 2", dst2)
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
-    cv2.destroyAllWindows()
-    cv2.waitKey(1)
-    
-
-def measureBox(bonus):
+def measureBox():
     global first, pos1, pos2
     first = True
     pos1 = pos2 = None
 
-    if bonus:
-        pass
-    else:
-        leftImage = openImage("../data/MorpheusL.jpg")
-        rightImage = openImage("../data/MorpheusR.jpg")
-        rotation = getRotation(MORPHEUS_LEFT_ROTATION, MORPHEUS_RIGHT_ROTATION)
-        translation = getTranslation(rotation, MORPHEUS_LEFT_TRANSLATION, MORPHEUS_RIGHT_TRANSLATION)
+    leftImage = openImage("MorpheusL.jpg")
+    rightImage = openImage("MorpheusR.jpg")
+    rotation = getRotation(MORPHEUS_LEFT_ROTATION, MORPHEUS_RIGHT_ROTATION)
+    translation = getTranslation(rotation, MORPHEUS_LEFT_TRANSLATION, MORPHEUS_RIGHT_TRANSLATION)
 
-        r1, r2, p1, p2, q, _, _ = cv2.stereoRectify(MORPHEUS_LEFT_CAMERA, None, MORPHEUS_RIGHT_CAMERA, None,
-                                                    leftImage.shape[0:2], rotation, translation, flags=0)
+    r1, r2, p1, p2, q, _, _ = cv2.stereoRectify(MORPHEUS_LEFT_CAMERA, None, MORPHEUS_RIGHT_CAMERA, None,
+                                                leftImage.shape[0:2], rotation, translation, flags=0)
 
-        map1x, map1y = cv2.initUndistortRectifyMap(MORPHEUS_LEFT_CAMERA, None, r1, p1, leftImage.shape[0:2], cv2.CV_32FC1)
-        map2x, map2y = cv2.initUndistortRectifyMap(MORPHEUS_RIGHT_CAMERA, None, r2, p2, leftImage.shape[0:2], cv2.CV_32FC1)
-        leftReprojection = cv2.remap(leftImage, map1x, map1y, cv2.INTER_LINEAR)
-        rightReprojection = cv2.remap(rightImage, map2x, map2y, cv2.INTER_LINEAR)
+    map1x, map1y = cv2.initUndistortRectifyMap(MORPHEUS_LEFT_CAMERA, None, r1, p1, leftImage.shape[0:2], cv2.CV_32FC1)
+    map2x, map2y = cv2.initUndistortRectifyMap(MORPHEUS_RIGHT_CAMERA, None, r2, p2, leftImage.shape[0:2], cv2.CV_32FC1)
+    leftReprojection = cv2.remap(leftImage, map1x, map1y, cv2.INTER_LINEAR)
+    rightReprojection = cv2.remap(rightImage, map2x, map2y, cv2.INTER_LINEAR)
 
-        dispMatrix, depthMatrix, corMatrix, worldCoords = tuneParameters("r3", leftReprojection, rightReprojection, q)
+    dispMatrix, depthMatrix, corMatrix, worldCoords = tuneParameters("r3", leftReprojection, rightReprojection, q)
 
-        cv2.namedWindow('Imagem', cv2.WINDOW_NORMAL)
-        cv2.setMouseCallback('Imagem', getMeasure, [leftReprojection, worldCoords])
+    cv2.namedWindow('Imagem', cv2.WINDOW_NORMAL)
+    cv2.setMouseCallback('Imagem', getMeasure, [leftReprojection, worldCoords])
 
-        while(True):
-            cv2.imshow("Imagem", leftReprojection)
-            if cv2.waitKey(1) & 0xFF == 27:
-                break
-        cv2.destroyAllWindows()
-        cv2.waitKey(1)
+    while(True):
+        cv2.imshow("Imagem", leftReprojection)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
 
 
 def getMeasure(event, x, y, flags, params):
@@ -300,18 +237,15 @@ def tuneParameters(req, leftImage, rightImage, q=None):
 
 
 def main(r1, r2, r3, imageL=None,
-         imageR=None, bonus=None):
+         imageR=None):
     if r1:
         return createDepthMap(imageL, imageR)
     elif r2:
-        if bonus:
-            depthFromFundamentalMatrix()
-        else:
-            return depthFromHomography()
+        return depthFromHomography()
     elif r3:
-        measureBox(bonus)
+        measureBox()
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    main(args.r1, args.r2, args.r3, args.img_l, args.img_r, args.bonus)
+    main(args.r1, args.r2, args.r3, args.img_l, args.img_r)
